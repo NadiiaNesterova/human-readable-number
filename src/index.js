b9a722c699d1701ada8009acdbf9abd019cdbee6
Nadiia Nesterova <nesterova1717@gmail.com>
2022-12-22 18:30:36 +0200

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-module.exports = function toReadable (number) {
-  const a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
-               'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  const b = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+module.exports = function toReadable(number) {
+  let numberStr = String(number);
+  let a = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  let b = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let c = ['ten', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let rem = number % 10;
+  let rem2 = number % 100;
+  let numRem = number - rem;
+  let numRem2 = number - rem2;
+  let s = rem2 - rem;
 
-  return (number < 20)? a[number]:
-  (number < 100)? b[Math.floor(number / 10)] + ((number % 10 === 0) ? '' : ' ' + a[number % 10]):
-   a[Math.floor(number / 100)] + ' hundred' + ((number % 100 === 0) ? '' : ' ' + toReadable([number % 100]));
+  return (number < 10)? a[number]:
+  (number > 9 && number < 20)? b[number - 10]:
+  (number > 19 && number < 100 && numberStr[numberStr.length - 1] === '0')? c[number / 10]:
+  (number > 19 && number < 100 && numberStr[numberStr.length - 1] !== '0')? `${c[numRem / 10]} ${a[number - numRem]}`:
+  (number >= 100 && number < 1000 && numberStr[numberStr.length - 1] === '0' && numberStr[numberStr.length - 2] === '0')? `${a[numRem2 / 100]} hundred`:
+  (number >= 100 && number < 1000 && numberStr[numberStr.length - 1] !== '0' && numberStr[numberStr.length - 2] === '1')? `${a[numRem2 / 100]} hundred ${b[rem2 - 10]}`:
+  (number >= 100 && number < 1000 && numberStr[numberStr.length - 1] !== '0' && numberStr[numberStr.length - 2] !== '0')? `${a[numRem2 / 100]} hundred ${c[s / 10]} ${a[rem]}`:
+  (number >= 100 && number < 1000 && numberStr[numberStr.length - 1] !== '0' && numberStr[numberStr.length - 2] === '0')? `${a[numRem2 / 100]} hundred ${a[rem]}`:
+  `${a[numRem2 / 100]} hundred ${c[s / 10]}`;
 }
